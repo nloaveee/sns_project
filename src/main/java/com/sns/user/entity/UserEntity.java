a99dcdb3ba0d4ae701c8fce927daba9e29df622a
test <test@test.com>
2024-07-12 18:49:45 +0900

    환경 세팅

---
+++ src/main/java/com/sns/user/entity/UserEntity.java
@@ -0,0 +1,53 @@
+package com.sns.user.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import groovy.transform.builder.Builder;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Table(name="user")
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UserEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "loginId")
+	private String loginId;
+	
+	private String password;
+	
+	private String name;
+	
+	private String email;
+	
+	@Column(name = "profileImageUrl")
+	private String profileImageUrl;
+	
+	@Column(name = "createdAt")
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+	
+	@Column(name = "updatedAt")
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+	
+}
